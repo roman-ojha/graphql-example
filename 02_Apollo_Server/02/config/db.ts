43fd0f5ebaac5e0cd0561309543995f2546acdcd
Roman-Ojha <razzroman98@gmail.com>
2022-12-05 12:06:54 +0545

    build: Init for 02/Apollo_Server

---
+++ 02_Apollo_Server/02/config/db.ts
@@ -0,0 +1,21 @@
+import pg from "pg";
+
+const client = new pg.Client({
+  user: process.env.DATABASEUSER,
+  host: process.env.DATABASEHOST,
+  database: process.env.DATABASENAME,
+  password: process.env.DATABASEPASSWORD,
+  port: parseInt(process.env.DATABASEPORT as string),
+});
+
+const connect = () => {
+  client
+    .connect()
+    .then(() => {
+      console.log("Database connected");
+    })
+    .catch((err) => console.log(err));
+};
+
+export default client;
+export { connect };
